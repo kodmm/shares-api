dffd6079bb1f4aed07df7ac6b0a7ee998df296f9
kodmm <53547701+kodmm@users.noreply.github.com>
2021-11-07 19:46:28 +0900

    add search function based on keyword && getDetailTv function

--- src/routes/api/v1/Tv.ts
+++ src/routes/api/v1/Tv.ts
@@ -1,7 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from 'express';
-
-
+import axios from "axios";
+import logger from "@shared/Logger";
+// import Tv from '@entities/searches/Tv';
 /**
  * Get any tv
  * @param req
@@ -10,9 +11,71 @@ import { Request, Response } from 'express';
  * @returns
  */
 
-export async function getAnyTvs(req: Request, res: Response, query: String) {
+export const getAnyTv = async(req: Request, res: Response) => {
+    const query = await req.query.query as string | undefined;
+    // baseUrl
+    const baseUrl: string = "https://image.tmdb.org/t/p";
+    const imgWidth: string = "/w500";
+
+    let resSearchTv: any;
     // 1. SearchでTvIDを取得
-    // 2. 1.で取得したIDを使用してTvのGetDetailsを取得
-    // 3. translationsを使用してoverviewsを日本語化する 
-    // 4. creditsを使用して出演者を取得
+    const url = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.TMDB_API_KEY}&language=jp-JP&query=${query}`;
+    const enUrl = encodeURI(url);
+    await axios.get(enUrl)
+        .then(response => resSearchTv = response.data);
+    console.log(resSearchTv);
+    return res.json({ resSearchTv, baseUrl: baseUrl + imgWidth});
+}
+
+/**
+ * 
+ * @param req 
+ * @param res 
+ * @returns
+ */
+export const getTvDetail = async(req: Request, res: Response) => {
+    const id = req.params.id;
+    //baseUrl
+    const baseUrl: string = "https://image.tmdb.org/t/p";
+    const imgWidth: string = "/w500";
+
+    let resDetail, resCredits, data: any = null;
+    let resOverviews: any;
+
+    //Tvの詳細データを取得
+    const url = `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.TMDB_API_KEY}&append_to_response=videos,images`;
+    await axios.get(url)
+        .then(response => resDetail = response.data);
+    
+    data = resDetail;
+
+    // translationsを使用してoverviewsを日本語化する
+    const urlTrsn = `https://api.themoviedb.org/3/tv/${id}/translations?api_key=${process.env.TMDB_API_KEY}`;
+    await axios.get(urlTrsn)
+        .then(response => resOverviews = response.data);
+    
+    data.overview = await overviewTrans(resOverviews.translations)
+
+    // creditsを使用して出演者を取得
+    const urlCrd = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${process.env.TMDB_API_KEY}&language=jp-JP`;
+    await axios.get(urlCrd)
+        .then(response => resCredits = response.data)
+    console.log(resCredits);
+    return res.json({ data, credits: resCredits, baseUrl: baseUrl + imgWidth });
+}
+
+
+/**
+ * 
+ * @param translations 
+ * @returns 
+ */
+const overviewTrans = async(translations: Array<any>) => {
+    const isoCode = "JP";
+    const result: any = await translations.filter(translation => translation.iso_3166_1 === isoCode)
+    console.log(result[0].data.overview);
+    if (result.length < 0) {
+        return translations[0].data.overview;
+    }
+    return result[0].data.overview;
 }

a54452f5299434376a5e11b8da6545f574fda2eb
kodmm <53547701+kodmm@users.noreply.github.com>
2021-11-25 13:27:21 +0900

    add used maybe function

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { getAnyTv, getTvDetail } from './Tv';
 
+
+
+
+
+
 // Tv-route
 const tvRouter = Router();
 tvRouter.get('/search', getAnyTv);
 tvRouter.get('/:id', getTvDetail);
-
+// tvRouter.get('/:id/:roomId',)
 // Export the base-router
 const baseRouter = Router();
 baseRouter.use('/tv', tvRouter);

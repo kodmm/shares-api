596196bc676d5124a49934b84b8f30125deacc98
kodmm <53547701+kodmm@users.noreply.github.com>
2022-01-19 16:16:22 +0900

    add setting Oauth of Twitter

--- src/auth/passportTwitterSSO.ts
+++ src/auth/passportTwitterSSO.ts
@@ -1,15 +1,21 @@
 import passport from 'passport';
 import TwitterStrategy from 'passport-twitter';
 
+// @ts-ignore
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_API_KEY,
     consumerSecret: process.env.TWITTER_API_SECRET,
-    // callbackURL: 'http://localhost:3000/mypage',
-    callbackURL: 'http://localhost:3001/auth/twiter/callback'
-
+    callbackURL: 'http://localhost:3001/api/v1/auth/twitter/callback',
+    includeEmail: true
 },(token: any, tokenSecret: any, profile: any, done: any) => {
+    console.log("function callback")
     // dbへ登録処理(find or create)
+    console.log("token:", token)
+    console.log("tokenSecret:", tokenSecret)
+    console.log("profile:", profile)
     done(null, token)
 
 }
 ))
+
+export default passport;

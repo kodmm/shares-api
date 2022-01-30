d91ad56a785687a13ff799342ef56da997a62ed0
kodmm <53547701+kodmm@users.noreply.github.com>
2022-01-31 03:03:23 +0900

    create model User

---
+++ migrations/20220130064442-create-user.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      domain: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      accessToken: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      refreshToken: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID4
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    domain: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    accessToken:  {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    refreshToken: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};

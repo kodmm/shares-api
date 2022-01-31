43e21bcd2b5a2241b6e285c56470776ac1f9107e
kodmm <53547701+kodmm@users.noreply.github.com>
2022-01-31 14:11:33 +0900

    add Insert User Data

--- src/auth/passportSSO.ts
+++ src/auth/passportSSO.ts
@@ -2,18 +2,36 @@ import passport from 'passport';
 import TwitterStrategy from 'passport-twitter';
 import GoogleStrategy from 'passport-google-oauth20';
 // @ts-ignore
+import db from '../../models/index.js'
+
+// @ts-ignore
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_API_KEY,
     consumerSecret: process.env.TWITTER_API_SECRET,
     callbackURL: 'http://localhost:3001/api/v1/auth/twitter/callback',
     includeEmail: true
-},(token: any, tokenSecret: any, profile: any, done: any) => {
+},async (token: any, tokenSecret: any, profile: any, done: any) => {
     console.log("function callback")
     // dbへ登録処理(find or create)
-    console.log("token:", token)
-    console.log("tokenSecret:", tokenSecret)
-    console.log("profile:", profile)
-    done(null, token)
+    const [user, created] = await db.User.findOrCreate({
+        where: {email: profile.emails[0].value},
+        defaults: { 
+            displayName: profile.displayName, 
+            email: profile.emails[0].value, 
+            photo: profile.photos[0].value,
+            provider: profile.provider,
+            accessToken: token,
+        },
+    })
+    console.log("-----------------")
+    console.log(user.toJSON())
+    console.log("-----------------")
+    done(null, { 
+        displayName: profile.displayName, 
+        email: profile.emails[0].value, 
+        photo: profile.photos[0].value,
+        provider: profile.provider,
+    })
 
 }
 ))
@@ -29,7 +47,7 @@ passport.use(new GoogleStrategy({
     console.log("token:", token)
     console.log("tokenSecret:", tokenSecret)
     console.log("profile:", profile)
-    done(null, token)
+    done(null, { ...profile, token: token })
 }))
 
 export default passport;

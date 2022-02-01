c6ea6cc00d5e796946a5443023032e8ca67980a4
kodmm <53547701+kodmm@users.noreply.github.com>
2022-02-01 10:27:53 +0900

    reafactor: add findorCreate function

--- src/auth/passportSSO.ts
+++ src/auth/passportSSO.ts
@@ -11,21 +11,12 @@ passport.use(new TwitterStrategy({
     callbackURL: 'http://localhost:3001/api/v1/auth/twitter/callback',
     includeEmail: true
 },async (token: any, tokenSecret: any, profile: any, done: any) => {
-    console.log("function callback")
     // dbへ登録処理(find or create)
-    const [user, created] = await db.User.findOrCreate({
-        where: {email: profile.emails[0].value},
-        defaults: { 
-            displayName: profile.displayName, 
-            email: profile.emails[0].value, 
-            photo: profile.photos[0].value,
-            provider: profile.provider,
-            accessToken: token,
-        },
-    })
-    console.log("-----------------")
+
+    const [user, created] = await findOrCreateUser(profile, token)
+
     console.log(user.toJSON())
-    console.log("-----------------")
+
     done(null, { 
         displayName: profile.displayName, 
         email: profile.emails[0].value, 
@@ -41,13 +32,31 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'http://localhost:3001/api/v1/auth/google/callback'
-},(token: any, tokenSecret: any, profile: any, done: any) => {
-    console.log("function callback")
+},async (token: any, tokenSecret: any, profile: any, done: any) => {
     // dbへ登録処理(find or create)
+    const [user, created] = await findOrCreateUser(profile, token)
+    console.log("--------", created)
     console.log("token:", token)
     console.log("tokenSecret:", tokenSecret)
     console.log("profile:", profile)
     done(null, { ...profile, token: token })
 }))
+
+
+const findOrCreateUser = async(profile: any, token: string) => {
+    const [user, created] = await db.User.findOrCreate({
+        where: {email: profile.emails[0].value},
+        defaults: { 
+            displayName: profile.displayName, 
+            email: profile.emails[0].value, 
+            photo: profile.photos[0].value,
+            provider: profile.provider,
+            accessToken: token,
+        },
+    })
+
+    return [user, created]
+
+}
 
 export default passport;

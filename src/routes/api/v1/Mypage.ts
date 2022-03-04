7fb8d7184f07d1c9dad48b1ac8569db888fb8c1c
kodmm <53547701+kodmm@users.noreply.github.com>
2022-03-05 01:29:20 +0900

    fixed get streaming data inside of getMyData function

--- src/routes/api/v1/Mypage.ts
+++ src/routes/api/v1/Mypage.ts
@@ -1,16 +1,26 @@
 import { StatusCodes } from "http-status-codes";
 import type { Request, Response } from 'express';
 import { IUser } from '@entities/users/User';
+import { IStreamingServiceData } from '@entities/tvs/Tv'
 import { IWatchActorsVideos } from '@entities/watches/Watch'
 import { findWatchesVideosActors } from './Watch';
+import { getStreamingServices } from './Tv';
 // @ts-ignore
 import db from '../../../../models'
 
 export const getMyData = async(req: Request, res: Response) => {
     const authUser: IUser | null = res.locals.authUser
     if (authUser) {
-        const watches: IWatchActorsVideos | null = await findWatchesVideosActors(authUser.id);
-        return res.json({ data: { user: authUser, watches: watches } })
+        const watches: { Watches: IWatchActorsVideos[] }| null= await findWatchesVideosActors(authUser.id);
+        if(watches !== null) {
+            const resWatches: IWatchActorsVideos[] = await Promise.all(watches.Watches.map(async(watch) => {
+                const streaming: IStreamingServiceData = await getStreamingServices(watch.Video.id)
+                watch.Video.streaming = streaming
+                return watch
+            }))
+            return res.json({ data: { user: authUser, watches: resWatches } })
+        }
+        return res.json({ data: { user: authUser, watches: null } })
     }
     return res.json({ data: { user: null  } })
 }
